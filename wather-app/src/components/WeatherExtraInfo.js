5b68a1296028506aeff4e76f7c60f3331f7f3d8d
manu <manuelsastre@metricool.com>
2019-10-13 18:42:53 +0200

    first commit

---
+++ wather-app/src/components/WeatherExtraInfo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WheaterExtraInfo = () => (
+    <div>Informacion extra</div>
+);
+
+export default WheaterExtraInfo;
---
+++ wather-app/src/components/WeatherTemperature.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WheaterTemperature = () =>(
+    <div><span>25 Grados</span></div>
+);
+
+export default WheaterTemperature;
